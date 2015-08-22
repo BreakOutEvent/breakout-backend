37bc5646c9377d4dd3534236bd24452d94f22679
Florian Schmidt <florian.schmidt.1994@icloud.com>
2015-08-22 19:05:39 +0200

    Add custom greeting for index

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Welcome to Breakout API. Nothing to see here by now").as("text/plain");
     }
 
 }
