918deb99391fed6df9ef4b13221530e77fbbd83d
Florian Schmidt <florian.schmidt.1994@icloud.com>
2015-11-01 21:02:26 +0100

    Move to SpringBoot instead of the Play Framework
    This commit will:
    * add a basic structure of a spring boot application according to the guide found on https://spring.io/guides/gs/actuator-service/
    * set the port of the application to 8080 in application.properties file
    * include some management REST endpoints shipped with spring boot actuator

---
+++ src/main/java/backend/BackendConfiguration.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendConfiguration.class, args);
+    }
+
+}
