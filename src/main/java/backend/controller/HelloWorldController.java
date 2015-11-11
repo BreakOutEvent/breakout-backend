b03d2033be22f2b0f5a51e8f02c5e0ceea4dc11b
Florian Schmidt <florian.schmidt.1994@icloud.com>
2015-11-11 17:58:24 +0100

    Revert "Add http basic authentication using spring-boot-starter-security"
    This reverts commit d4062edd073619bf6db69e95c6474b4705c91454.

--- src/main/java/backend/controller/HelloWorldController.java
+++ src/main/java/backend/controller/HelloWorldController.java
@@ -2,17 +2,21 @@ package backend.controller;
 
 import backend.model.Greeting;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@Controller
+@RequestMapping("/helloworld")
 public class HelloWorldController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/helloworld")
+    @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody Object sayHello(@RequestParam(value="name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
