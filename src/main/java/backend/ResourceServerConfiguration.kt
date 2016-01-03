95d4d3d16ea0e81010d6a6295334e4bfc555e4c5
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-01-03 15:14:38 +0100

    Convert ResourceServer- and WebSecurityConfiguration to Kotlin
    A bug in the previous versions of Kotlin prevented this.

---
+++ src/main/java/backend/ResourceServerConfiguration.kt
@@ -0,0 +1,31 @@
+package backend
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+
+/*
+ * The ResourceServerConfigurerAdapter is an adapter for the interface for @EnableResourceServer classes.
+ * Implement this interface to adjust the access rules and paths that are protected by OAuth2 security.
+ * Applications may provide multiple instances of this interface, and in general (like with other Security configurers),
+ * if more than one configures the same preoperty, then the last one wins.
+ * The configurers are sorted by Order before being applied.
+ *
+ * From http://docs.spring.io/spring-security/oauth/apidocs/org/springframework/security/oauth2/config/annotation/web/configuration/ResourceServerConfigurer.html
+ */
+
+@Configuration
+@EnableResourceServer
+open class ResourceServerConfiguration : ResourceServerConfigurerAdapter() {
+
+    override fun configure(resources: ResourceServerSecurityConfigurer?) {
+        resources!!.resourceId("BREAKOUT_BACKEND")
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity?) {
+        http!!.authorizeRequests().antMatchers("*").permitAll()
+    }
+}
---
+++ src/main/java/backend/WebSecurityConfiguration.kt
@@ -0,0 +1,29 @@
+package backend
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebMvcSecurity
+open class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    private val userDetailsService: CustomUserDetailsService? = null
+
+    @Throws(Exception::class)
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth!!.userDetailsService(userDetailsService).passwordEncoder(BCryptPasswordEncoder())
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+}
