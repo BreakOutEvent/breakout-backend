565476f72bb2789d24c83bf1a6c1063e4a3e2bcf
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-01-31 21:04:17 +0100

    Embed EmailAddress in Invitation

--- src/main/java/backend/model/event/Invitation.kt
+++ src/main/java/backend/model/event/Invitation.kt
@@ -2,12 +2,15 @@ package backend.model.event
 
 import backend.model.BasicEntity
 import backend.model.misc.EmailAddress
+import javax.persistence.Embedded
 import javax.persistence.Entity
 
 @Entity
 class Invitation() : BasicEntity() {
 
     lateinit var status: InvitationStatus
+
+    @Embedded
     lateinit var invitee: EmailAddress
 
     constructor(email: EmailAddress) : this() {
