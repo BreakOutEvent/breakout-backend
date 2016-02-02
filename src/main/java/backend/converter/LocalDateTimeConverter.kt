2a2fe5a4b7ba02729c562c72a3e42abcad0e0e31
Philipp Piwowarsky <piwo.mail@googlemail.com>
2016-02-02 23:04:29 +0100

    LocalDateTime converter for JPA

---
+++ src/main/java/backend/converter/LocalDateTimeConverter.kt
@@ -0,0 +1,18 @@
+package backend.converter
+
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import javax.persistence.AttributeConverter
+import javax.persistence.Converter
+
+@Converter(autoApply = true)
+class LocalDateTimeConverter() : AttributeConverter<LocalDateTime, Timestamp> {
+
+    override fun convertToDatabaseColumn(data: java.time.LocalDateTime): java.sql.Timestamp {
+        return java.sql.Timestamp.valueOf(data);
+    }
+
+    override fun convertToEntityAttribute(data: java.sql.Timestamp): java.time.LocalDateTime {
+        return data.toLocalDateTime();
+    }
+}
