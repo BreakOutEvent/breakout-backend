bab2698349afd73e0914c07d057d9e7d79dc619f
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-03-03 15:49:41 +0100

    Use ConfigurationService in MailServiceImpl

--- src/main/java/backend/services/MailServiceImpl.kt
+++ src/main/java/backend/services/MailServiceImpl.kt
@@ -4,7 +4,6 @@ import backend.model.misc.Email
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -17,22 +16,19 @@ import org.springframework.web.util.UriComponentsBuilder
 @Profile("!test")
 class MailServiceImpl : MailService {
 
-    @Value("\${org.breakout.mailer.url}")
-    private lateinit var url: String
-
-    @Value("\${org.breakout.mailer.port}")
-    private lateinit var port: String
-
-    @Value("\${org.breakout.mailer.xauthtoken}")
-    private lateinit var token: String
-
-    private lateinit var restTemplate: RestOperations
+    private val url: String
+    private val port: String
+    private val token: String
+    private val restTemplate: RestOperations
 
     private val logger = Logger.getLogger(MailServiceImpl::class.java)
 
     @Autowired
-    constructor(restTemplate: RestOperations) {
+    constructor(restTemplate: RestOperations, configurationService: ConfigurationService) {
         this.restTemplate = restTemplate
+        this.port = configurationService.getRequired("org.breakout.mailer.port")
+        this.token = configurationService.getRequired("org.breakout.mailer.xauthtoken")
+        this.url = configurationService.getRequired("org.breakout.mailer.url")
     }
 
     override fun send(email: Email) {
@@ -45,6 +41,7 @@ class MailServiceImpl : MailService {
         try {
             restTemplate.exchange(getSendUrl(url, port), HttpMethod.POST, request, String::class.java)
         } catch (e: Exception) {
+            // TODO: Implement better fallback in this case!
             logger.error("Mailer not available at this time")
         }
 
