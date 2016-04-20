ce3b8c873ff076f252ef89bbab337a7e5db77426
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-04-20 10:35:30 +0200

    Auto apply MoneyConverter for Entities

--- src/main/java/backend/converter/MoneyConverter.kt
+++ src/main/java/backend/converter/MoneyConverter.kt
@@ -4,7 +4,7 @@ import org.javamoney.moneta.Money
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
-@Converter
+@Converter(autoApply = true)
 class MoneyConverter() : AttributeConverter<Money, String> {
 
     override fun convertToDatabaseColumn(attribute: Money): String {
