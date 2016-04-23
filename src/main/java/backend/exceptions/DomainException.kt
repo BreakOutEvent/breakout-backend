a276d45a0a104ba41418642a2854b9c8c1efc97b
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-04-23 16:37:34 +0200

    Handle DomainExceptions properly
    The reason this did not work before properly was because
    the exception handlers do only work on RuntimeExceptions
    but not on unchecked exceptions.
    
    Closes #77

--- src/main/java/backend/exceptions/DomainException.kt
+++ src/main/java/backend/exceptions/DomainException.kt
@@ -1,10 +1,6 @@
 package backend.exceptions;
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class DomainException : Exception {
+class DomainException : RuntimeException {
 
     constructor() : super()
 
