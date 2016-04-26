aa815790120c5d553381bcc970961e199208c19f
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-04-26 19:13:02 +0200

    Send emails on payment
    Closes #96

--- src/main/java/backend/model/payment/TeamEntryFeeServiceImpl.kt
+++ src/main/java/backend/model/payment/TeamEntryFeeServiceImpl.kt
@@ -1,6 +1,9 @@
 package backend.model.payment
 
+import backend.model.misc.Email
+import backend.model.misc.EmailAddress
 import backend.model.user.Admin
+import backend.services.MailService
 import org.apache.log4j.Logger
 import org.javamoney.moneta.Money
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,21 +14,49 @@ import javax.transaction.Transactional
 class TeamEntryFeeServiceImpl : TeamEntryFeeService {
 
     private val teamEntryFeeInvoiceRepository: TeamEntryFeeInvoiceRepository
+    private val mailService: MailService
     private val logger: Logger
 
     @Autowired
-    constructor(teamEntryFeeInvoiceRepository: TeamEntryFeeInvoiceRepository) {
+    constructor(teamEntryFeeInvoiceRepository: TeamEntryFeeInvoiceRepository, mailService: MailService) {
         this.teamEntryFeeInvoiceRepository = teamEntryFeeInvoiceRepository
         this.logger = Logger.getLogger(TeamEntryFeeServiceImpl::class.java)
+        this.mailService = mailService
     }
 
     @Transactional
     override fun addAdminPaymentToInvoice(admin: Admin, amount: Money, invoice: TeamEntryFeeInvoice): TeamEntryFeeInvoice {
         val payment = AdminPayment(amount, admin)
         invoice.addPayment(payment)
+
+        val email = if (invoice.isFullyPaid()) {
+            getFullyPaidEmail(invoice)
+        } else {
+            getPartiallyPaidEmail(invoice)
+        }
+
+        mailService.send(email, false)
         return invoice
-        //TODO: Make sure emails are sent here!
     }
+
+    // TODO: Make Payment amount human readable
+    private fun getPartiallyPaidEmail(invoice: TeamEntryFeeInvoice) = Email(
+            to = invoice.team!!.members.map { EmailAddress(it.email) },
+            subject = "BreakOut 2016 - Eine Zahlung ist bei uns eingegangen",
+            body = "Hallo Team ${invoice.team!!.name},<br><br>" +
+                    "Eine neue Zahlung für euer Team ist bei uns eingegangen. Ihr habt damit ${invoice.amountOfCurrentPayments()} von ${invoice.amount} bezahlt!<br><br>" +
+                    "Vielen Dank<br>" +
+                    "Euer BreakOut-Team" // TODO: Replace me
+    )
+
+    private fun getFullyPaidEmail(invoice: TeamEntryFeeInvoice) = Email(
+            to = invoice.team!!.members.map { EmailAddress(it.email) },
+            subject = "BreakOut 2016 - Startgebühr vollständig bezahlt",
+            body = "Hallo Team ${invoice.team!!.name},<br><br>" +
+                    "Eine neue Zahlung für euer Team ist bei uns eingegangen. Ihr habt damit die Startgebühr von ${invoice.amount} vollständig bezahlt!<br><br>" +
+                    "Viel Spaß und viel Erfolg!<br>" +
+                    "Euer BreakOut-Team" // TODO: Replace me
+    )
 
     @Transactional
     override fun save(invoice: TeamEntryFeeInvoice): TeamEntryFeeInvoice {
