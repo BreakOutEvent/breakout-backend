2804b8bbe2ad62f2596b62b8fa1a5f1e666a310b
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-05-16 06:32:36 +0200

    Challenges can be created with un-/registered sponsor
    Closes #121, #125

---
+++ src/main/java/backend/controller/ChallengeController.kt
@@ -0,0 +1,131 @@
+package backend.controller
+
+import backend.configuration.CustomUserDetails
+import backend.controller.exceptions.BadRequestException
+import backend.controller.exceptions.NotFoundException
+import backend.controller.exceptions.UnauthorizedException
+import backend.model.challenges.Challenge
+import backend.model.challenges.ChallengeService
+import backend.model.event.Team
+import backend.model.event.TeamService
+import backend.model.posting.PostingService
+import backend.model.sponsoring.UnregisteredSponsor
+import backend.model.user.Participant
+import backend.model.user.Sponsor
+import backend.model.user.User
+import backend.model.user.UserService
+import backend.util.euroOf
+import backend.view.UnregisteredSponsorView
+import org.javamoney.moneta.Money
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMethod.POST
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@RestController
+open class ChallengeController {
+
+    private var challengeService: ChallengeService
+    private var postingService: PostingService
+    private var teamService: TeamService
+    private var userService: UserService
+
+    @Autowired
+    constructor(challengeService: ChallengeService, userService: UserService, teamService: TeamService, postingService: PostingService) {
+        this.challengeService = challengeService
+        this.userService = userService
+        this.teamService = teamService
+        this.postingService = postingService
+    }
+
+    /**
+     * POST /event/{eventId}/team/{teamId}/challenge/
+     * Propose a challenge to a team. This can only be done
+     * when being a sponsor or when providing data for an unregistered sponsor
+     */
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("/event/{eventId}/team/{teamId}/challenge/", method = arrayOf(POST))
+    @ResponseStatus(CREATED)
+    open fun createChallengeS(@AuthenticationPrincipal customUserDetails: CustomUserDetails,
+                              @PathVariable teamId: Long,
+                              @Valid @RequestBody body: ChallengeView): ChallengeView {
+
+        val user = userService.getUserFromCustomUserDetails(customUserDetails)
+        val team = teamService.findOne(teamId) ?: throw NotFoundException("No team with id $teamId found")
+        val amount = euroOf(body.amount!!)
+        val description = body.description!!
+
+        if (user.hasRole(Sponsor::class)) return challengeWithRegisteredSponsor(user, team, amount, description)
+        else if (user.hasRole(Participant::class)) return challengeUnregisteredSponsor(body, team, amount, description)
+        else throw UnauthorizedException("User in neither Participant nor sponsor")
+    }
+
+    private fun challengeWithRegisteredSponsor(user: User, team: Team, amount: Money, description: String): ChallengeView {
+        val sponsor = user.getRole(Sponsor::class) ?: throw UnauthorizedException("User is no sponsor")
+        val challenge = challengeService.proposeChallenge(sponsor, team, amount, description)
+        return ChallengeView(challenge)
+    }
+
+    private fun challengeUnregisteredSponsor(body: ChallengeView, team: Team, amount: Money, description: String): ChallengeView {
+        val unregisteredSponsor = body.unregisteredSponsor ?: throw BadRequestException("Missing data for unregistered sponsor")
+
+        val sponsor = UnregisteredSponsor(
+                firstname = unregisteredSponsor.firstname!!,
+                lastname = unregisteredSponsor.lastname!!,
+                company = unregisteredSponsor.company!!,
+                gender = unregisteredSponsor.gender!!,
+                url = unregisteredSponsor.url!!,
+                address = unregisteredSponsor.address!!.toAddress()!!,
+                isHidden = unregisteredSponsor.isHidden)
+
+        val challenge = challengeService.proposeChallenge(sponsor, team, amount, description)
+        return ChallengeView(challenge)
+    }
+}
+
+class ChallengeStatusView {
+    var status: String? = null
+    var postingId: Long? = null
+}
+
+
+class ChallengeView {
+
+    var status: String? = null
+
+    var teamId: Long? = null
+
+    var team: String? = null
+
+    var sponsorId: Long? = null
+
+    @Valid
+    var unregisteredSponsor: UnregisteredSponsorView? = null
+
+    @NotNull
+    var amount: Double? = null
+
+    @NotNull
+    @Size(max = 1000)
+    var description: String? = null
+
+    /**
+     * no-args constructor for Jackson
+     */
+    constructor()
+
+    constructor(challenge: Challenge) {
+        this.description = challenge.description
+        this.amount = challenge.amount.numberStripped.toDouble()
+        this.sponsorId = challenge.sponsor?.id
+        this.teamId = challenge.team!!.id!!
+        this.team = challenge.team!!.name
+        this.status = challenge.status.toString()
+        this.unregisteredSponsor = challenge.unregisteredSponsor?.let { UnregisteredSponsorView(it) }
+    }
+}
---
+++ src/test/java/backend/controller/ChallengeControllerTest.kt
@@ -0,0 +1,103 @@
+package backend.controller
+
+import backend.Integration.IntegrationTest
+import backend.Integration.getTokens
+import backend.Integration.toJsonString
+import backend.model.misc.Coord
+import backend.model.user.Participant
+import backend.model.user.Sponsor
+import org.junit.Before
+import org.junit.Test
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDateTime
+
+class ChallengeControllerTest : IntegrationTest() {
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+    }
+
+    @Test
+    fun testCreateChallengeWithRegisteredSponsor() {
+
+        val event = eventService.createEvent("title", LocalDateTime.now(), "city", Coord(0.0, 1.1), 36)
+        val participant = userService.create("participant@break-out.org", "password", { addRole(Participant::class) }).getRole(Participant::class)!!
+        val sponsor = userService.create("sponsor@break-out.org", "password", { addRole(Sponsor::class) }).getRole(Sponsor::class)!!
+        val team = teamService.create(participant, "name", "description", event)
+        val tokens = getTokens(this.mockMvc, sponsor.email, "password")
+
+        val body = mapOf(
+                "amount" to 100,
+                "description" to "Jump into lake titicaca naked"
+        ).toJsonString()
+
+        val request = MockMvcRequestBuilders.post("/event/${event.id}/team/${team.id}/challenge/")
+                .header("Authorization", "Bearer ${tokens.first}")
+                .contentType(APPLICATION_JSON_UTF_8)
+                .content(body)
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated)
+                .andExpect(jsonPath("$.description").value("Jump into lake titicaca naked"))
+                .andExpect(jsonPath("$.sponsorId").value(sponsor.id!!.toInt()))
+                .andExpect(jsonPath("$.amount").value(100.0))
+                .andExpect(jsonPath("$.unregisteredSponsor").doesNotExist())
+                .andExpect(jsonPath("$.team").value(team.name))
+                .andExpect(jsonPath("$.teamId").value(team.id!!.toInt()))
+                .andExpect(jsonPath("$.status").value("PROPOSED"))
+    }
+
+    @Test
+    fun testCreateChallengeWithUnregisteredSponsor() {
+        val event = eventService.createEvent("title", LocalDateTime.now(), "city", Coord(0.0, 1.1), 36)
+        val participant = userService.create("participant@break-out.org", "password", { addRole(Participant::class) }).getRole(Participant::class)!!
+        userService.create("sponsor@break-out.org", "password", { addRole(Sponsor::class) }).getRole(Sponsor::class)!!
+        val team = teamService.create(participant, "name", "description", event)
+        val tokens = getTokens(this.mockMvc, participant.email, "password")
+
+        val body = mapOf(
+                "amount" to 100,
+                "description" to "Do something really awesome, because you know you are awesome!",
+                "unregisteredSponsor" to mapOf(
+                        "firstname" to "Hans",
+                        "lastname" to "Meier",
+                        "company" to "privat",
+                        "url" to "",
+                        "gender" to "male",
+                        "isHidden" to "false",
+                        "address" to mapOf(
+                                "street" to "street",
+                                "housenumber" to "123",
+                                "zipcode" to "0000",
+                                "city" to "City",
+                                "country" to "Germany"
+                        )
+                )
+        ).toJsonString()
+
+        val request = MockMvcRequestBuilders.post("/event/${event.id}/team/${team.id}/challenge/")
+                .header("Authorization", "Bearer ${tokens.first}")
+                .contentType(APPLICATION_JSON_UTF_8)
+                .content(body)
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated)
+                .andExpect(jsonPath("$.description").value("Do something really awesome, because you know you are awesome!"))
+                .andExpect(jsonPath("$.sponsorId").doesNotExist())
+                .andExpect(jsonPath("$.amount").value(100.0))
+                .andExpect(jsonPath("$.team").value(team.name))
+                .andExpect(jsonPath("$.teamId").value(team.id!!.toInt()))
+                .andExpect(jsonPath("$.status").value("ACCEPTED"))
+                .andExpect(jsonPath("$.unregisteredSponsor").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.firstname").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.lastname").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.company").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.url").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.gender").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.hidden").exists())
+                .andExpect(jsonPath("$.unregisteredSponsor.address").exists())
+    }
+}
