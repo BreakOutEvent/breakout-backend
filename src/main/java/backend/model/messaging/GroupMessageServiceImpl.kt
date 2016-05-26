a1c1ead131d4334505d979dd846f5857b3fd589e
Philipp Piwo <piwo.mail@googlemail.com>
2016-05-26 21:30:46 +0200

    email for new user of a groupmessage
    closes #147

--- src/main/java/backend/model/messaging/GroupMessageServiceImpl.kt
+++ src/main/java/backend/model/messaging/GroupMessageServiceImpl.kt
@@ -1,12 +1,15 @@
 package backend.model.messaging
 
+import backend.model.misc.Email
+import backend.model.misc.EmailAddress
 import backend.model.user.UserCore
+import backend.services.MailService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class GroupMessageServiceImpl @Autowired constructor(val repository: GroupMessageRepository) : GroupMessageService {
+class GroupMessageServiceImpl @Autowired constructor(val repository: GroupMessageRepository, val mailService: MailService) : GroupMessageService {
 
     @Transactional
     override fun createGroupMessage(creator: UserCore): GroupMessage {
@@ -21,6 +24,20 @@ class GroupMessageServiceImpl @Autowired constructor(val repository: GroupMessag
     @Transactional
     override fun addUser(user: UserCore, groupMessage: GroupMessage): GroupMessage {
         groupMessage.addUser(user)
+
+        val email = Email(
+                to = listOf(EmailAddress(user.email)),
+                subject = "BreakOut 2016 - Euch wurde eine Challenge gestellt!",
+                body = "Hallo ${user.firstname ?: ""},<br><br>" +
+                        "Du wurdest einer neuen Gruppennachricht hinzugefügt. Sieh nach was man Dir mitteilen mag!<br><br>" +
+                        "Liebe Grüße<br>" +
+                        "Euer BreakOut-Team",
+                buttonText = "NACHRICHT ÖFFNEN",
+                buttonUrl = "https://anmeldung.break-out.org/messages/${groupMessage.id}?utm_source=backend&utm_medium=email&utm_content=intial&utm_campaign=add_groupmessage",
+                campaignCode = "add_groupmessage"
+        )
+
+        mailService.send(email)
 
         return repository.save(groupMessage)
     }
