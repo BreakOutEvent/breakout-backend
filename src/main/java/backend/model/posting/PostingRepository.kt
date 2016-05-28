96f0d8cbcd0a6ed1b40d5344e2cd9d8f47b08a76
Philipp Piwo <piwo.mail@googlemail.com>
2016-05-28 18:35:58 +0200

    sort postings by hashtag by id

--- src/main/java/backend/model/posting/PostingRepository.kt
+++ src/main/java/backend/model/posting/PostingRepository.kt
@@ -11,7 +11,7 @@ interface PostingRepository : CrudRepository<Posting, Long> {
     @Query("from Posting where id > :id order by id desc")
     fun findAllSince(@Param("id") id: Long): Iterable<Posting>
 
-    @Query("select p from Posting p inner join p.hashtags h where h.value = :hashtag")
+    @Query("select p from Posting p inner join p.hashtags h where h.value = :hashtag order by p.id desc")
     fun findByHashtag(@Param("hashtag") hashtag: String): Iterable<Posting>
 
     fun findAll(pageable: Pageable): Iterable<Posting>
