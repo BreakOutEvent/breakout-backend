59e353dc7601c4445ea0a46dfa66ad347c2a996a
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-06-02 11:55:46 +0200

    Add unique constraint to authentication_id of oauth_access_token

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,7 +5,7 @@
 
 CREATE TABLE oauth_client_details (client_id VARCHAR(30) PRIMARY KEY, resource_ids VARCHAR(256),  client_secret VARCHAR(256),  scope VARCHAR(256),  authorized_grant_types VARCHAR(256),  web_server_redirect_uri VARCHAR(256),  authorities VARCHAR(256),  access_token_validity INTEGER,  refresh_token_validity INTEGER,  additional_information VARCHAR(256), autoapprove VARCHAR(256));
 CREATE TABLE oauth_client_token (token_id VARCHAR(256),  token BLOB,  authentication_id VARCHAR(256),  user_name VARCHAR(256),  client_id VARCHAR(256));
-CREATE TABLE oauth_access_token (token_id VARCHAR(256),  token BLOB,  authentication_id VARCHAR(256),  user_name VARCHAR(256),  client_id VARCHAR(256),  authentication BLOB,  refresh_token VARCHAR(256));
+CREATE TABLE oauth_access_token (token_id VARCHAR(256),  token BLOB,  authentication_id VARCHAR(256),  user_name VARCHAR(256),  client_id VARCHAR(256),  authentication BLOB,  refresh_token VARCHAR(256), UNIQUE(authentication_id));
 CREATE TABLE oauth_refresh_token (token_id VARCHAR(256),  token BLOB,  authentication BLOB);
 CREATE TABLE oauth_code (code VARCHAR(256), authentication BLOB);
 
