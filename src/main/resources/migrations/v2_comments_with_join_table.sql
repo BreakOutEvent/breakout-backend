6e16de077822a40a738e1b5830836c4f32344962
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-01-31 17:36:51 +0100

    Add missing semicolon to migration

--- src/main/resources/migrations/v2_comments_with_join_table.sql
+++ src/main/resources/migrations/v2_comments_with_join_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE `posting_comments` (
   CONSTRAINT `FK_e24nwt6jvxa7kbxnx5i5qjl64` FOREIGN KEY (`comments_id`) REFERENCES `comment` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO posting_comments (SELECT posting_id AS posting_id, id AS comment_id FROM comment)
+INSERT INTO posting_comments (SELECT posting_id AS posting_id, id AS comment_id FROM comment);
 
 -- DROP FK for posting_id as they are now in the join column
 ALTER TABLE comment DROP FOREIGN KEY FK_cb78ra5e8th78whr91f8kl5gg;
