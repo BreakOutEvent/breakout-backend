9145d1ce4ab066968f43277f632953d94ec2ac87
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-03-30 10:07:37 +0200

    Create invoice with purposeOfTransfer for each team

---
+++ src/main/java/backend/controller/TeamControllerV2.kt
@@ -0,0 +1,45 @@
+package backend.controller
+
+import backend.configuration.CustomUserDetails
+import backend.controller.exceptions.NotFoundException
+import backend.controller.exceptions.UnauthorizedException
+import backend.exceptions.DomainException
+import backend.model.event.TeamService
+import backend.model.user.Participant
+import backend.model.user.UserService
+import backend.view.TeamEntryFeeInvoiceView
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/team")
+open class TeamControllerV2 @Autowired constructor(
+        val userService: UserService,
+        val teamService: TeamService
+) {
+
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping("/{teamId}/startingfee")
+    open fun getInvoiceForTeam(@PathVariable teamId: Long,
+                          @AuthenticationPrincipal customUserDetails: CustomUserDetails): TeamEntryFeeInvoiceView {
+
+        val user = userService.getUserFromCustomUserDetails(customUserDetails)
+        val team = teamService.findOne(teamId) ?: throw NotFoundException("Team with id $teamId not found")
+
+        user.getRole(Participant::class)?.let {
+            if (team.isMember(it)) {
+                val invoice = team.invoice ?: throw DomainException("Team has no invoice")
+                return TeamEntryFeeInvoiceView(invoice)
+            } else {
+                throw UnauthorizedException("Participant is not a member of team $teamId")
+            }
+        }
+
+        throw UnauthorizedException("User is no participant")
+    }
+}
