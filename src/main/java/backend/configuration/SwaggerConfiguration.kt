4db333a6d5f72e6a68683cbc3e1964dc4822da46
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-04-15 15:00:17 +0200

    Remove swagger springfox deprectation warning

--- src/main/java/backend/configuration/SwaggerConfiguration.kt
+++ src/main/java/backend/configuration/SwaggerConfiguration.kt
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors.regex
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger.web.UiConfiguration
@@ -33,10 +35,18 @@ class SwaggerConfiguration {
     @Bean
     fun uiConfiguration() = UiConfiguration.DEFAULT
 
-    private fun apiInfo() = ApiInfoBuilder()
-            .title("Breakout Backend REST API")
-            .description("This is the description of the REST API for the Breakout Backend")
-            .version("1.1.0").contact("florian.schmidt@break-out.org, philipp.piwowarsky@break-out.org")
-            .license("GNU AGPL v3").licenseUrl("https://www.gnu.org/licenses/agpl-3.0.de.html")
-            .build()
+    private fun apiInfo(): ApiInfo? {
+        val florian = Contact("Florian Schmidt", "break-out.org", "florian.schmidt@break-out.org")
+        val philipp = Contact("Philipp Piwowarsky", "break-out.org", "philipp.piwowarsky@break-out.org")
+
+        return ApiInfoBuilder()
+                .title("Breakout Backend REST API")
+                .description("This is the description of the REST API for the Breakout Backend")
+                .version("1.1.0")
+                .contact(florian)
+                .contact(philipp)
+                .license("GNU AGPL v3")
+                .licenseUrl("https://www.gnu.org/licenses/agpl-3.0.de.html")
+                .build()
+    }
 }
