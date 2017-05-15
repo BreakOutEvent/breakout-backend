87e7c257e31e53c201e0bc45a651d2a425278789
Tom Graupner <tom.graupner@breakout.com>
2017-05-15 10:58:54 +0200

    fixed @Profile of FakeNotificationService

--- src/main/java/backend/services/fakeservices/FakeNotificationService.kt
+++ src/main/java/backend/services/fakeservices/FakeNotificationService.kt
@@ -4,14 +4,13 @@ import backend.model.messaging.Message
 import backend.model.user.UserAccount
 import backend.services.NotificationService
 import backend.util.Profiles.TEST
-import backend.util.Profiles.DEVELOPMENT
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
 @Service
-@Profile(DEVELOPMENT, TEST)
+@Profile(TEST)
 class FakeNotificationService: NotificationService {
 
     val logger: Logger = LoggerFactory.getLogger(FakeNotificationService::class.java)
