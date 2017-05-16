8f35c75b0cbe819a6b6dde537fb992fe8205f28b
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-05-17 00:23:28 +0200

    Revert "fixed @Profile of FakeNotificationService"
    This commit did not solve the actual problem. Has been
    solved on branch develop.
    This reverts commit 87e7c257e31e53c201e0bc45a651d2a425278789.

--- src/main/java/backend/services/fakeservices/FakeNotificationService.kt
+++ src/main/java/backend/services/fakeservices/FakeNotificationService.kt
@@ -4,13 +4,14 @@ import backend.model.messaging.Message
 import backend.model.user.UserAccount
 import backend.services.NotificationService
 import backend.util.Profiles.TEST
+import backend.util.Profiles.DEVELOPMENT
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
 @Service
-@Profile(TEST)
+@Profile(DEVELOPMENT, TEST)
 class FakeNotificationService: NotificationService {
 
     val logger: Logger = LoggerFactory.getLogger(FakeNotificationService::class.java)
