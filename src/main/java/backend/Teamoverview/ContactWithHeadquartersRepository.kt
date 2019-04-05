3664f74baaaad380197658a3b549fab20cbdaf70
Mathias Quintero <me@quintero.io>
2019-04-05 14:05:04 +0200

    Adding contact with hq repo

---
+++ src/main/java/backend/Teamoverview/ContactWithHeadquartersRepository.kt
@@ -0,0 +1,19 @@
+package backend.Teamoverview
+
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+
+interface ContactWithHeadquartersRepository : CrudRepository<ContactWithHeadquarters, Long> {
+
+    fun findAllByTeamId(teamId: Long): Iterable<ContactWithHeadquarters>
+
+    @Query("""
+        select *
+        from contact_with_headquarters
+        where team_id = :id
+        order by id desc
+        limit 1
+    """, nativeQuery = true)
+    fun findLastContactByTeamId(@Param("id") id: Long): ContactWithHeadquarters?
+}
