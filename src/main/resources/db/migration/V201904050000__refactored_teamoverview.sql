e1d7d9f2863eae6ba8ba3aedda22d093f741acd6
Mathias Quintero <me@quintero.io>
2019-04-05 14:07:27 +0200

    Adding migration

---
+++ src/main/resources/db/migration/V201904050000__refactored_teamoverview.sql
@@ -0,0 +1,15 @@
+
+--  (╯°□°）╯︵ ┻━┻ I said a droppa da table
+drop table team_overview_location_data;
+drop table team_overview_members;
+drop table team_overview;
+
+CREATE TABLE `contact_with_headquarters` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `team_id` bigint(20) NOT NULL,
+  `admin_id` bigint(20) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
