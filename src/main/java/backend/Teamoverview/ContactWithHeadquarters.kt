109c4297eea9cd8163d770a1464b27025440190b
Mathias Quintero <me@quintero.io>
2019-04-05 14:04:37 +0200

    Adding new ContactWithHeadquarters entity

---
+++ src/main/java/backend/Teamoverview/ContactWithHeadquarters.kt
@@ -0,0 +1,28 @@
+package backend.Teamoverview
+
+import backend.model.BasicEntity
+import backend.model.event.Team
+import backend.model.user.UserAccount
+import javax.persistence.*
+
+@Entity
+class ContactWithHeadquarters : BasicEntity {
+
+    @Column(columnDefinition = "TEXT")
+    var comment: String? = null
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var admin: UserAccount? = null
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var team: Team? = null
+
+    private constructor() : super()
+
+    constructor(team: Team, comment: String, admin: UserAccount) {
+        this.team = team
+        this.comment = comment
+        this.admin = admin
+    }
+
+}
