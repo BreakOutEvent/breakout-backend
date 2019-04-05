195cbc171845464af0e5d33bd0d34ade664cf224
Mathias Quintero <me@quintero.io>
2019-04-05 14:06:47 +0200

    Adding latest changes to team overview controller

--- src/main/java/backend/Teamoverview/TeamOverviewController.kt
+++ src/main/java/backend/Teamoverview/TeamOverviewController.kt
@@ -1,7 +1,10 @@
 package backend.Teamoverview
 
+import backend.configuration.CustomUserDetails
+import backend.model.user.UserService
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
@@ -9,7 +12,8 @@ import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/teamoverview/")
-class TeamOverviewController(val teamOverviewService: TeamOverviewService) {
+class TeamOverviewController(val teamOverviewService: TeamOverviewService,
+                             val userService: UserService) {
 
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -17,12 +21,21 @@ class TeamOverviewController(val teamOverviewService: TeamOverviewService) {
         return teamOverviewService.findAll()
     }
 
+    @GetMapping("{teamId}/calls/")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    fun getCallsForTeam(@PathVariable teamId: Long): Iterable<ContactWithHeadquarters> {
+        return teamOverviewService.allCalls(teamId)
+    }
+
     @PostMapping("{teamId}/lastContactWithHeadquarters/")
     @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(CREATED)
     fun addContactCommentForTeam(@PathVariable teamId: Long,
-                                 @Valid @RequestBody body: ContactCommentBody) {
-        teamOverviewService.addComment(teamId, body.comment)
+                                 @Valid @RequestBody body: ContactCommentBody,
+                                 @AuthenticationPrincipal customUserDetails: CustomUserDetails) {
+
+        val admin = userService.getUserFromCustomUserDetails(customUserDetails)
+        teamOverviewService.addComment(teamId, body.comment, admin.account)
     }
 
     class ContactCommentBody {
