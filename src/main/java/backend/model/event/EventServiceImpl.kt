6a21931d11ce49c31f329b9f2f772d1bdd099fdd
Philipp Piwo <github@piwo.space>
2019-08-03 17:05:27 +0200

    make event calculation slower

--- src/main/java/backend/model/event/EventServiceImpl.kt
+++ src/main/java/backend/model/event/EventServiceImpl.kt
@@ -26,7 +26,7 @@ class EventServiceImpl @Autowired constructor(val repository: EventRepository,
 
     val logger: Logger = LoggerFactory.getLogger(EventServiceImpl::class.java)
 
-    @Scheduled(cron = "*/30 * * * * ?")
+    @Scheduled(cron = "0 */5 * * * ?")
     @Transactional(propagation = Propagation.REQUIRED)
     fun scheduleRegenerateScores() {
         if (featureFlagService.isEnabled("event.scheduleRegenerateScores"))
