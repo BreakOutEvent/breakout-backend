a3f0d132243f8a6d5cc866fb7ee9fa8f692811e0
Dominik Glandorf <do.gl@gmx.de>
2021-03-29 21:58:57 +0200

    calculate sum of distances

--- src/main/java/backend/model/location/Location.kt
+++ src/main/java/backend/model/location/Location.kt
@@ -8,7 +8,6 @@ import backend.model.posting.Posting
 import backend.model.user.Participant
 import backend.util.distanceCoordsKM
 import backend.util.distanceCoordsListKM
-import backend.model.location.LocationRepository.getAllLatLonByTeamId
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -50,9 +49,8 @@ class Location : BasicEntity {
         this.uploader = uploader
         this.date = date
         this.distance = distanceCoordsKM(from = team!!.event.startingLocation, to = coord)
-        if (team!!.event.id == 14) {
-            this.distance = distanceCoordsListKM(
-                getAllLatLonByTeamId(team!!.id).map(Coord(it.latitude, it.longitude)))
+        if (team!!.event.id!!.equals(20)) {
+            this.distance = distanceCoordsListKM(this.team!!.locations.map{Coord(it.coord.latitude, it.coord.longitude)} + coord)
         }
         this.locationData = locationData
     }
