7feab2dcaf7e2ab81446530039719bca7771bddf
Sebastian Schubotz <sebastian.schubotz@we-do.ai>
2022-04-11 20:09:48 +0200

    Make user emails accessible via JSON #388

--- src/main/java/backend/view/InvitationView.kt
+++ src/main/java/backend/view/InvitationView.kt
@@ -5,9 +5,11 @@ import backend.model.event.Invitation
 class InvitationView() {
     var team: Long? = null
     var name: String? = null
+    var email: String = ""
 
     constructor(invitation: Invitation) : this() {
         this.team = invitation.team?.id
         this.name = invitation.team?.name
+        this.email = invitation.invitee.value
     }
 }
--- src/main/java/backend/view/user/BasicUserView.kt
+++ src/main/java/backend/view/user/BasicUserView.kt
@@ -25,6 +25,10 @@ class BasicUserView() {
     @SafeHtml(whitelistType = NONE)
     var gender: String? = null
 
+    @Valid
+    @SafeHtml(whitelistType = NONE)
+    var email: String? = null
+
     var id: Long? = null
 
     var isBlocked: Boolean? = null
@@ -40,6 +44,7 @@ class BasicUserView() {
         this.user = user
         this.firstname = user.firstname
         this.lastname = user.lastname
+        this.email = user.email
         this.id = user.account.id
         this.gender = user.gender
         this.isBlocked = user.isBlocked
